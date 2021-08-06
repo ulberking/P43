d1e35efa7b2044046d04f06938b64c74081dd104
ulberking <83320350+ulberking@users.noreply.github.com>
2021-08-05 20:26:36 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+     
+fill("white")
+textSize(15)
+text("R E V B A I L A", 700,150)
+fill("red")
+text("Hint: Don't forget to call it !!", 700,170)  
+   
+fill("white")
+textSize(15)
+text("R E V B A I L A", 100,250)
+fill("green")
+text("Hint: store of all !!", 100,270)
+}
